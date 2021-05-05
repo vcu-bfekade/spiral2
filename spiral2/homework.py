7c5ab2766489ca5518607896afafc16551022843
Bruk Fekade <fekadeb2@vcu.edu>
2021-05-04 20:07:51 -0400

    Update homework.py

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,4 +1,18 @@
 def spiralize(size, n=1):
     """ Your code goes somewhere in here"""
+    n = 1
+    adder = 0
+    rome = 2
+    totl = 0
+
+    while n <= size ** 2:
+        totl += 1
+        n += rome
+        adder += 1
+        if adder == n:
+            rome += 2
+            adder = 0
+    return totl
+
     return_value = n
     return return_value
